7797d3fda9250fed74b969a96209d5bd6a53f517
Saketku98 <sankeshsingh@gmail.com>
2022-01-01 23:01:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+//write a program to have 3 variables and output the highest of all time
+alert("This Program is to check the greatest number amongst all")
+let num1= parseFloat(prompt("Enter the first number"))
+let num2= parseFloat(prompt("Enter the second number"))
+let num3= parseFloat(prompt("Enter the third number"))
+
+  if (num1 > num2 && num1 > num3)
+  { console.log("num1");
+    alert(num1 + " is Greater")
+    }
+    else if (num2 > num1 && num2 > num3)
+  { console.log("num2");
+    alert(num2 + " is Greater")
+    }
+    else {
+    console.log("num3");
+    alert(num3 + " is Greater")
+} 
+alert(number  + " is Greater Than 10")
---
+++ q1.js
@@ -0,0 +1,11 @@
+alert("This Program Checks if the Entered Number Is greater Than 10")
+let number= parseFloat(prompt("Enter the number to check if it is greater than 10"))
+
+if(number>10){
+    console.log(number  + " is Greater Than 10");
+    alert(number  + " is Greater Than 10")
+}
+else{
+    console.log(number + " is not Greater than 10");
+    alert(number + " is not Greater than 10")
+}
